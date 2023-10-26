9a76bd97c083870d7b70c2533e4ccdece874f517
SaraYohannes <sara.yohannes@hesbynett.no>
2023-10-26 11:33:51 +0200

    new project structure
    modeled after a bridge pattern

---
+++ Assets/Scripts/TreeStructure.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeStructure : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
