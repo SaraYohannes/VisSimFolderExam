aa3100299c31614eabee76d2d56ba2f7c6117a8a
SaraYohannes <sara.yohannes@hesbynett.no>
2023-10-26 13:10:28 +0200

    update

---
+++ Assets/Scripts/TreeNode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeNode : MonoBehaviour
+{
+    /// tree node
+    // max amount of data stored inside the node
+    // pointer to children
+    // actuall array or vector with data
+}
--- Assets/Scripts/TreeStructure.cs
+++ Assets/Scripts/TreeStructure.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class TreeStructure : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// quad structure
+    [SerializeField] public TreeNode Node;
+    // get radius from pointCloudProcesser
+    [SerializeField] public Vector3 boundLength;
+    private TreeNode Root;
+    // start
+    private void Start()
+    {
+        Root = GetComponent<TreeNode>(); // root of tree
+        // set up node        
+        BuildTree();
+    }
+    // update tree?    
+    private void Update()
     {
         
+    }
+    // build tree (algorithm)
+    void BuildTree()
+    {
+        // check if tree is done
     }
 
-    // Update is called once per frame
-    void Update()
+    // functionality
+    void insertNode()
     {
-        
+        // insert
     }
+    void removeNode()
+    {
+        // remove
+    }
+    void searchNode()
+    {
+        // search
+
+    }
+
 }
