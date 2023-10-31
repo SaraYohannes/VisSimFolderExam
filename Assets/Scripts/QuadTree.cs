0c6cebe7b2909678f3eebc97c02c27e973bf2166
SaraYohannes <sara.yohannes@hesbynett.no>
2023-10-31 18:51:09 +0100

    clean up update

--- Assets/Scripts/QuadTree.cs
+++ Assets/Scripts/QuadTree.cs
@@ -40,50 +40,6 @@ public class QuadTree
             south = null;
             west = null;
         }
-
-        /*public Node north, east, south, west;
-        public int treeHeight, nodeHeight;
-        List<Vector3> datapoints = new List<Vector3>();
-        public float minX, minZ, maxX, maxZ;
-        Vector3 rep_point;
-        bool b_emptyList;
-        public Node(float minx, float maxx, float minz, float maxz, int nodeheight, int treeheight)
-        {
-            minX = minx;
-            minZ = minz;
-            maxX = maxx;
-            maxZ = maxz;
-            nodeHeight = nodeheight;
-            treeHeight = treeheight;
-            north = null;
-            east = null;
-            south = null;
-            west = null;
-        }
-        //~Node() 
-        //{ 
-        
-        //}
-        //void NodeValueRange()
-        //{
-
-        //}
-        //void NodeRepValue()
-        //{
-        //    /// find average of all Z values in datapoints
-        //}
-        //void MoreHeight(Vector3[] nodeChildren, int treeHeight, int nodeHeight)
-        //{
-        //    ///travel inside each child and make another set of children unless nodeHeight=> than treeheight
-        //    /*if (nodeHeight < treeHeight)
-        //    {
-        //        for (int i = 0; i < nodeChildren.Length; i++)
-        //        {
-        //            nodeChildren[i] = CreateNode();// nodeChildren[i] = new QuadTree.Node(2f, 2f, minX, maxX, minZ, maxZ, nodeHeight, treeHeight);
-
-        //        }
-        //    }
-        //}*/
     }
 
     public class Tree
@@ -99,7 +55,6 @@ public class QuadTree
             int nodeheight = 0;
             insertNodes(nodeheight, treeheight, minX, maxX, minZ, maxZ, rootNode);
             Debug.Log("QuadTree:Tree:Tree: Tree was just successfully finished!");
-
         }
         QuadTree.Node insertNodes(int nodeheight, int treeheight, float minX, float maxX, float minZ, float maxZ, QuadTree.Node root)
         {
@@ -135,116 +90,5 @@ public class QuadTree
         {
             
         }
-            /*
-            // NORTH
-            // X = XMIN -> XMID
-            // Z = ZMID -> ZMAX
-
-            // EAST
-            // X = XMID -> XMAX
-            // Z = ZMID -> ZMAX
-
-            // SOUTH
-            // X = XMID -> XMAX
-            // Z = ZMIN -> ZMID
-
-            // WEST
-            // X = XMIN -> XMID
-            // Z = ZMIN -> ZMID
-            */
-            /*int counter = 0;
-            if (root == null)
-            {
-                root = new QuadTree.Node(minX, maxX, minZ, maxZ, counter);
-            }
-            if (treeheight == counter)
-            {
-                return new QuadTree.Node(minX, maxX, minZ, maxZ, counter);
-            }
-            float xmin, xmax, zmin, zmax, xmid, zmid;
-            xmin = minX; xmax = maxX;
-            zmin = minZ; zmax = maxZ;
-            // for as long as the nodes children aren't yet the correct height, continue making nodes and inserting them
-            while (root.nodeHeight<treeheight)
-            {
-                // initialize node with correct x/z values
-                QuadTree.Node next = null;
-                // find x and z values
-                xmid = xRange(xmin, xmax);
-                zmid = zRange(zmin, zmax);
-                // new max values for next loop
-                xmax = xmid; zmax = zmid; 
-                counter++;
-            }*/
-        /*public Tree(Vector3[] data_points, int treeheight, float minX, float maxX, float minZ, float maxZ)
-        {
-            Debug.Log("QuadTree:Tree:Tree: A class was successfully constructed");
-            rootNode = null;
-
-            int nodeCounter = FinalNumberPerfectNodes(treeheight);
-            int NodeHeight = 0;
-            float minx, minz, maxx, maxz;
-            minx = minX;
-            minz = minZ;
-            maxx = maxX;
-            maxz = maxZ;
-            while (treeheight > NodeHeight)
-            {
-
-                // what x/z ranges does the node have?
-                // x/z values for each node
-                float midX = xRange(minx, maxx);
-                float midZ = zRange(minz, maxz);
-                /*
-                // 0<=x<0.5 ==> minX <= x < (maxX/2)
-                    // 0<=y<0.5 ==> minZ <= z < (maxZ/2)
-                    // 0.5<=y<1 ==> (maxZ/2) <= z < maxZ
-                // 0.5<=x<1 ==> (maxX/2) <= x < maxX
-                    // 0<=y<0.5 ==> minZ <= z < (maxZ/2)
-                    // 0.5<=y<1 ==> (maxZ/2) <= z < maxZ
-                
-                // which height is it on?
-                NodeHeight = NodeHeightCalculator(NodeHeight, treeheight, nodeCounter);
-                InsertNode(NodeHeight, treeheight, minX, maxX, minZ, maxZ);
-            }
-        }
-        float xRange(float min, float max)
-        {
-            return (max - min) / 2f;
-        }
-        float zRange(float min, float max)
-        {
-            return (max - min) / 2f;
-        }
-        int FinalNumberPerfectNodes(int treeheight) ///LOGIC SHOULD BE FINE
-        {
-            int nodeCounter = 1;
-            if (treeheight == 0)
-                return 1;
-            else
-            {
-                for (int i = 1; i < treeheight+1; i++)
-                {
-                    nodeCounter = nodeCounter * 4;
-                }
-            }
-            return nodeCounter;
-        }
-        int NodeHeightCalculator(int Nodeheight, int Treeheight, int nodeCounter)
-        {
-            
-            return Nodeheight;
-        }
-        void InsertNode(int nodeheight, int treeheight, float minX, float maxX, float minZ, float maxZ)
-        {
-            // if rootNode does not exist- make it
-                // increase NodeHeight
-                // if we have 1/4/16/...
-                // (previous number * 4, except if previous =0, then increase with 1
-
-
-            // add root and their appropriate x/z values and nodeheight
-        }
-        */
     }
 }
